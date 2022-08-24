18bbd460ebfda006192d38f215b08ff000252cbb
张森云 <zhangsy@citydo.com.cn>
2022-08-24 15:35:34 +0800

    fix:规划文本上传格式添加.pdf

--- src/views/PlanAudit/Save/index.vue
+++ src/views/PlanAudit/Save/index.vue
@@ -2,143 +2,80 @@
   <div>
     <el-form ref="form" :model="form" label-width="100px" label-position="top">
       <div class="block">
-        <div
-          class="text-lg cursor-pointer inline-block"
-          @click="$router.back()"
-        >
+        <div class="text-lg cursor-pointer inline-block" @click="$router.back()">
           <i class="el-icon-arrow-left"></i>
           <span> {{ typeMap[type] }}评审</span>
         </div>
         <div class="mt-4">
           <p class="ml-4 mb-2">报送规划设计公司</p>
-          <el-form-item
-            class="inline-block"
-            label="设计公司名称："
-            :rules="rule.input"
-            prop="companyName"
-          >
-            <el-input
-              class="input"
-              v-model="form.companyName"
-              :maxlength="50"
-              placeholder="请输入设计公司全称"
-            />
+          <el-form-item class="inline-block" label="设计公司名称：" :rules="rule.input" prop="companyName">
+            <el-input class="input" v-model="form.companyName" :maxlength="50" placeholder="请输入设计公司全称" />
           </el-form-item>
-          <el-form-item
-            class="inline-block"
-            label="单位资质："
-            :rules="rule.input"
-            prop="qualification"
-          >
-            <el-input
-              class="input"
-              v-model="form.qualification"
-              :maxlength="50"
-              placeholder="请输入"
-            />
+          <el-form-item class="inline-block" label="单位资质：" :rules="rule.input" prop="qualification">
+            <el-input class="input" v-model="form.qualification" :maxlength="50" placeholder="请输入" />
           </el-form-item>
-          <el-form-item
-            class="inline-block"
-            label="项目负责人："
-            :rules="rule.input"
-            prop="projectManager"
-          >
-            <el-input
-              class="input"
-              v-model="form.projectManager"
-              :maxlength="50"
-              placeholder="请输入"
-            />
+          <el-form-item class="inline-block" label="项目负责人：" :rules="rule.input" prop="projectManager">
+            <el-input class="input" v-model="form.projectManager" :maxlength="50" placeholder="请输入" />
           </el-form-item>
-          <el-form-item
-            class="inline-block"
-            label="县级负责人："
-            :rules="rule.input"
-            prop="countyManager"
-          >
-            <el-input
-              class="input"
-              v-model="form.countyManager"
-              placeholder="请输入"
-            />
+          <el-form-item class="inline-block" label="县级负责人：" :rules="rule.input" prop="countyManager">
+            <el-input class="input" v-model="form.countyManager" placeholder="请输入" />
           </el-form-item>
-<!--          <el-form-item-->
-<!--            class="inline-block"-->
-<!--            label="分数:"-->
-<!--            :rules="rule.input"-->
-<!--            prop="score"-->
-<!--          >-->
-<!--            <el-input-->
-<!--              class="input"-->
-<!--              type="number"-->
-<!--              v-model="form.score"-->
-<!--              placeholder="请输入评审分数"-->
-<!--              @change="computeGrade"-->
-<!--            />-->
-<!--          </el-form-item>-->
-<!--          <el-form-item-->
-<!--            class="inline-block"-->
-<!--            label="评审等次:"-->
-<!--            prop="grade"-->
-<!--          >-->
-<!--            <el-input-->
-<!--              class="input"-->
-<!--              v-model="form.grade"-->
-<!--              disabled-->
-<!--              placeholder="自动计算"-->
-<!--            />-->
-<!--          </el-form-item>-->
-          <el-form-item
-              label="规划文本："
-              :rules="rule.upload"
-              prop="planFilesArr"
-          >
+          <!--          <el-form-item-->
+          <!--            class="inline-block"-->
+          <!--            label="分数:"-->
+          <!--            :rules="rule.input"-->
+          <!--            prop="score"-->
+          <!--          >-->
+          <!--            <el-input-->
+          <!--              class="input"-->
+          <!--              type="number"-->
+          <!--              v-model="form.score"-->
+          <!--              placeholder="请输入评审分数"-->
+          <!--              @change="computeGrade"-->
+          <!--            />-->
+          <!--          </el-form-item>-->
+          <!--          <el-form-item-->
+          <!--            class="inline-block"-->
+          <!--            label="评审等次:"-->
+          <!--            prop="grade"-->
+          <!--          >-->
+          <!--            <el-input-->
+          <!--              class="input"-->
+          <!--              v-model="form.grade"-->
+          <!--              disabled-->
+          <!--              placeholder="自动计算"-->
+          <!--            />-->
+          <!--          </el-form-item>-->
+          <el-form-item label="规划文本：" :rules="rule.upload" prop="planFilesArr">
             <UploadFile
-                tip="支持格式：.ppt, .pptx"
-                accept=".ppt,.pptx"
-                :data="form.planFilesArr"
-                @add="onFileAdd($event, 'planFilesArr')"
-                @remove="onFileRemove($event, 'planFilesArr')"
+              tip="支持格式：.ppt, .pptx, .pdf"
+              accept=".ppt,.pptx,.pdf"
+              :data="form.planFilesArr"
+              @add="onFileAdd($event, 'planFilesArr')"
+              @remove="onFileRemove($event, 'planFilesArr')"
             />
           </el-form-item>
           <p class="ml-4 mb-2">县级规划评审情况</p>
-          <el-form-item
-              label="县级规划评审意见："
-              :rules="rule.input"
-              prop="suggestion"
-          >
-            <el-input
-                v-model="form.suggestion"
-                type="textarea"
-                :rows="5"
-                placeholder="请输入"
-            />
+          <el-form-item label="县级规划评审意见：" :rules="rule.input" prop="suggestion">
+            <el-input v-model="form.suggestion" type="textarea" :rows="5" placeholder="请输入" />
           </el-form-item>
-          <el-form-item
-              label="上传附件："
-              :rules="rule.upload"
-              prop="suggestionFilesArr"
-          >
+          <el-form-item label="上传附件：" :rules="rule.upload" prop="suggestionFilesArr">
             <UploadFile
-                tip="支持格式：.doc, .docx, .pdf"
-                accept=".doc,.docx,.pdf"
-                :data="form.suggestionFilesArr"
-                @add="onFileAdd($event, 'suggestionFilesArr')"
-                @remove="onFileRemove($event, 'suggestionFilesArr')"
+              tip="支持格式：.doc, .docx, .pdf"
+              accept=".doc,.docx,.pdf"
+              :data="form.suggestionFilesArr"
+              @add="onFileAdd($event, 'suggestionFilesArr')"
+              @remove="onFileRemove($event, 'suggestionFilesArr')"
             />
           </el-form-item>
           <p class="ml-4 mb-2">政府批复附件</p>
-          <el-form-item
-              label="上传政府批复附件："
-              :rules="rule.upload"
-              prop="approvalFilesArr"
-          >
+          <el-form-item label="上传政府批复附件：" :rules="rule.upload" prop="approvalFilesArr">
             <UploadFile
-                tip="支持格式：.doc, .docx,.pdf"
-                accept=".doc,.docx,.pdf"
-                :data="form.approvalFilesArr"
-                @add="onFileAdd($event, 'approvalFilesArr')"
-                @remove="onFileRemove($event, 'approvalFilesArr')"
+              tip="支持格式：.doc, .docx,.pdf"
+              accept=".doc,.docx,.pdf"
+              :data="form.approvalFilesArr"
+              @add="onFileAdd($event, 'approvalFilesArr')"
+              @remove="onFileRemove($event, 'approvalFilesArr')"
             />
           </el-form-item>
           <div class="text-center">
@@ -152,32 +89,32 @@
 </template>
 
 <script>
-import rule from "@/mixins/rule";
-import _ from "lodash";
-import { getPlanDetail, createPlan, modifyPlan } from "@/api/planningReview";
+import rule from '@/mixins/rule';
+import _ from 'lodash';
+import { getPlanDetail, createPlan, modifyPlan } from '@/api/planningReview';
 
 export default {
   mixins: [rule],
   data() {
     return {
-      id: "",
-      type: "",
+      id: '',
+      type: '',
       typeMap: {
-        add: "新增",
-        edit: "编辑",
-        view: "查看",
+        add: '新增',
+        edit: '编辑',
+        view: '查看',
       },
       form: {
         approvalFilesArr: [],
-        companyName: "",
-        qualification: "",
-        countyManager: "",
-        id: "",
+        companyName: '',
+        qualification: '',
+        countyManager: '',
+        id: '',
         planFilesArr: [],
-        projectManager: "",
-        suggestion: "",
+        projectManager: '',
+        suggestion: '',
         suggestionFilesArr: [],
-        villageDetailId: "",
+        villageDetailId: '',
         // score: "", // 分数
         // grade: "", // 等级
       },
@@ -193,14 +130,21 @@ export default {
     // 计算等级
     computeGrade() {
       switch (true) {
-        case this.form.score < 60: this.form.grade = '不合格'; break;
-        case this.form.score < 70: this.form.grade = '合格'; break;
-        case this.form.score < 85: this.form.grade = '良好'; break;
-        default: this.form.grade = '优秀';
+        case this.form.score < 60:
+          this.form.grade = '不合格';
+          break;
+        case this.form.score < 70:
+          this.form.grade = '合格';
+          break;
+        case this.form.score < 85:
+          this.form.grade = '良好';
+          break;
+        default:
+          this.form.grade = '优秀';
       }
     },
     async getDetail() {
-      if (this.type === "add") {
+      if (this.type === 'add') {
         return;
       }
       this.detail = await getPlanDetail(this.id);
@@ -224,26 +168,22 @@ export default {
       this.$refs.form.validate(async (valid) => {
         if (valid) {
           this.$myConfirm({
-            content: "是否确认提交",
+            content: '是否确认提交',
           }).then(async () => {
             const form = _.cloneDeep(this.form);
             form.villageDetailId = this.id;
             // 图片数组处理
-            const keyArray = [
-              "planFilesArr",
-              "suggestionFilesArr",
-              "approvalFilesArr",
-            ];
+            const keyArray = ['planFilesArr', 'suggestionFilesArr', 'approvalFilesArr'];
             keyArray.forEach((key) => {
               form[key] = form[key].map((item) => item.fileId);
             });
             try {
-              if (this.type === "add") {
+              if (this.type === 'add') {
                 await createPlan(form);
               } else {
                 await modifyPlan(form);
               }
-              this.$notify.success("提交成功");
+              this.$notify.success('提交成功');
               this.$router.back();
             } catch (err) {
               console.log(err);
